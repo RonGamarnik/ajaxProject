0da52b8372caf4897d206cc2e99feec6dcf1d7ae
polinakremneva <polinaapolly@gmail.com>
2024-05-27 12:58:39 +0300

    4.0

--- display/management/index.js
+++ display/management/index.js
@@ -62,7 +62,6 @@ async function addBookToLocalServer(book) {
 
 async function displayBooks(books) {
   const booksContainer = document.getElementById("books");
-booksContainer.style.opacity = "1";
   booksContainer.innerHTML = ""; // Clear previous results
   booksContainer.style.opacity = 1;
 
